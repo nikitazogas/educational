a93c1acb0ea0ae7aa304fec3b3583f02b6bb649a
nikita.zogas <nikita.zogas@gmail.com>
2014-01-03 00:07:52 +0200

    Modified initial version of educational HelloWorld

--- projectpackage/MyApp.java
+++ projectpackage/MyApp.java
@@ -2,6 +2,9 @@ package projectpackage;
 
 public class MyApp {
 	public static void main(String args[])	{
-		System.out.println("Hello world");
+
+		for (int i = 0; i < 5; i++) {
+			System.out.println(i+" iteration");
+		}
 	}
 }
